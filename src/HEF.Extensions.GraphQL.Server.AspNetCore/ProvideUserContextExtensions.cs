e8013cc2f3c825af685d7a98a736c8f4fb59e16f
万里涛 <wanlitao_3055601@outlook.com>
2020-03-20 19:18:37 +0800

    add resolve httpcontext from usercontext

---
+++ src/HEF.Extensions.GraphQL.Server.AspNetCore/ProvideUserContextExtensions.cs
@@ -0,0 +1,20 @@
+﻿using GraphQL.Execution;
+using Microsoft.AspNetCore.Http;
+using System;
+
+namespace GraphQL.Server.Transports.AspNetCore
+{
+    public static class ProvideUserContextExtensions
+    {
+        public static HttpContext GetHttpContext(this IProvideUserContext provideContext)
+        {
+            if (provideContext == null)
+                throw new ArgumentNullException(nameof(provideContext));
+
+            if (provideContext.UserContext is HttpContextUserContext httpUserContext)
+                return httpUserContext.Context;
+
+            throw new ArgumentException($"can't resolve HttpContext from target UserContext");
+        }
+    }
+}
